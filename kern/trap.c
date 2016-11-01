c56e4b00c872e2bf7b1a1bc0bb1bfaf1bea5a943
litong <14307130132@fudan.edu.cn>
2016-11-01 17:03:48 +0800

    adding handler for exception,but have some bug

--- kern/trap.c
+++ kern/trap.c
@@ -65,6 +65,41 @@ trap_init(void)
 	extern struct Segdesc gdt[];
 
 	// LAB 3: Your code here.
+	void handler0();
+	void handler1();
+	//void handler2();
+	void handler3();
+	void handler4();
+	void handler5();
+	void handler6();
+	void handler7();
+	void handler8();
+	void handler9();
+	void handler10();
+	void handler11();
+	void handler12();
+	void handler13();
+	void handler14();
+	void handler15();
+	void handler16();
+	SETGATE(idt[0],0,GD_KD,handler0,0);	
+	SETGATE(idt[1],0,GD_KD,handler1,0);
+	//SETGATE(idt[2],0,GD_KD,handler2,0);
+	SETGATE(idt[3],0,GD_KD,handler3,0);
+	SETGATE(idt[4],0,GD_KD,handler4,0);
+	SETGATE(idt[5],0,GD_KD,handler5,0);
+	SETGATE(idt[6],0,GD_KD,handler6,0);
+	SETGATE(idt[7],0,GD_KD,handler7,0);
+	SETGATE(idt[8],0,GD_KD,handler8,0);
+	SETGATE(idt[9],0,GD_KD,handler9,0);
+	SETGATE(idt[10],0,GD_KD,handler10,0);
+	SETGATE(idt[11],0,GD_KD,handler11,0);
+	SETGATE(idt[12],0,GD_KD,handler12,0);
+	SETGATE(idt[13],0,GD_KD,handler13,0);
+	SETGATE(idt[14],0,GD_KD,handler14,0);	
+	SETGATE(idt[15],0,GD_KD,handler15,0);
+	SETGATE(idt[16],0,GD_KD,handler16,0);
+
 
 	// Per-CPU setup 
 	trap_init_percpu();
